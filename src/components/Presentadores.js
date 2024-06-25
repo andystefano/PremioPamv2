7282877d141873a441ebd3a262777373cd38366c
Andy Hormazabal <andy@andy.cl>
2024-06-25 11:53:44 -0400

    add changes

--- src/components/Presentadores.js
+++ src/components/Presentadores.js
@@ -7,11 +7,11 @@ function Presentadores() {
         <h2 class="presentan__titulo">PRESENTA:</h2>
         <img src="img/PRESENTA.png" alt='Fundación Antenna' />
         <h2 class="presentan__titulo">AUSPICIA:</h2>
-        <img src="img/AUSPICIA.png" alt='bank Of America' />  
+        <img src="img/AUSPICIA.png" alt='Bank Of America' />  
         <h2 class="presentan__titulo">COLABORA:</h2>
         <img src="img/COLABORA2.png" alt='GRUPO ROCA'  />  
-        <img src="img/COLABORA1.png" alt='Capitulo Chileno de Mational Musseum of WOMEN in the ARTS' />  
         <h2 class="presentan__titulo">APOYA:</h2>
+        <img src="img/COLABORA1.png" alt='Capitulo Chileno de Mational Musseum of WOMEN in the ARTS' />  
         <img src="img/APOYA.png" alt='e-press' />                          
         </>
   );
