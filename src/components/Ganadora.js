d86f01d007aaa00c35a0b9f9ae4c9feff954ad48
ANDY Hormazabal <andy@andy.cl>
2024-09-02 02:19:29 -0400

    add

---
+++ src/components/Ganadora.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+
+function Ganadoras(props) {
+    return ( <div className="ganadora mt-14">
+        <div>
+          <div className="flex w-full">
+            <div className="pr-10 pt-4">
+              <img
+                src={`/img/ganadoras/${props.retrato}`}
+                alt={`Retrato`}
+                style={{ width: "345px" }}
+              />
+            </div>
+            <div>
+              <h2 class="contenido__tituloGanadoras w-full">
+                GANADORAS PAM {props.ano}
+              </h2>
+              <h1 class="titulo_principalGanadoras w-full">
+                { props.nombre }
+              </h1>
+              <p class="text-black w-full">
+                { props.biografia }
+              </p>
+            </div>
+          </div>
+          <div className="w-full">
+            <img
+              src={`/img/ganadoras/isabel_obra.png`}
+              style={{ width: "100%" }}
+              alt="Obra"
+              className="pt-10"
+            />
+          </div>
+        </div>
+        <div className="w-full">
+          <div className="pt-10">
+            <ol>
+              <li>
+                Título : Circuitos Híbridos Dimensiones : { props.titulo }
+              </li>
+              <li>Fecha de creación : { props.fecha }</li>
+              <li>
+                Técnica : { props.tecnica }
+              </li>
+              <li>
+                Dimensiones : { props.dimensiones }
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>)
+}
+
+export default Ganadoras;
--- src/components/GanadorasSection.js
+++ src/components/GanadorasSection.js
@@ -2,6 +2,7 @@ import React from "react";
 import { FaInstagram } from "react-icons/fa6";
 import { ImFacebook } from "react-icons/im";
 import { AiOutlineLinkedin } from "react-icons/ai";
+import Ganadoras from "./Ganadora"
 
 function GanadorasSection() {
   return (
@@ -19,48 +20,65 @@ function GanadorasSection() {
           </ul>
         </div>
         <div class="contenidoGanadoras ">
-          <div>
-            <div className="flex w-full">
-              <div className="pr-10 pt-4">
-                <img
-                  src="/img/ganadoras/isabel.png"
-                  style={{ width: "345px" }}
-                  alt="Isabel"
-                />
+
+
+
+
+
+          <Ganadoras nombre={'ISABEL DEL RIORIO'} biografia={"Isabel del Río, nacida en 1956, es una artista visual chilena, licenciada en Artes con especialización en grabado por la Pontificia Universidad Católica de Chile y magíster en Artes por la Universidad de Chile. Ha exhibido su trabajo en destacadas instituciones como la Bienal de Praga, el Blanton Museum of Art y el Haus Am Kleist-Park de Berlín."} retrato={"isabel.png"} obra={"isabel.png"}  titulo={"Circuitos Híbrido"} dimensiones={"410 x 100 cm"} fecha={"2007"} tecnica={"120 sistemas de audio programado y pregrabados y plancha de acrílico"} ano={2023}  />
+        
+          <div className="ganadora  mt-14">
+            <div>
+              <div className="flex w-full">
+                <div className="pr-10 pt-4" style={{minWidth:'355px'}}>
+                  <img
+                    src="/img/ganadoras/isabel.png"
+                    style={{ width: "345px" }}
+                    alt="Isabel"
+                  />
+                </div>
+                <div>
+                  <h2 class="contenido__tituloGanadoras w-full">
+                    GANADORAS PAM 2023
+                  </h2>
+                  <h1 class="titulo_principalGanadoras w-full">
+                    ISABEL DEL RIO
+                  </h1>
+                  <p class="text-black w-full">
+                    Fundación Antenna y Bank of America presentan la quinta
+                    versión del Premio PAM Artista Mujer, que invita a artistas
+                    visuales mujeres desde los 60 años.
+                  </p>
+                </div>
               </div>
-              <div>
-                <h2 class="contenido__tituloGanadoras w-full">
-                  GANADORAS PAM 2023
-                </h2>
-                <h1 class="titulo_principalGanadoras w-full">ISABEL DEL RIO</h1>
-                <p class="text-black w-full">
-                  Fundación Antenna y Bank of America presentan la quinta
-                  versión del Premio PAM Artista Mujer, que invita a artistas
-                  visuales mujeres desde los 60 años.
-                </p>
+              <div className="w-full">
+                <img
+                  src="/img/ganadoras/isabel_obra.png"
+                  style={{ width: "100%" }}
+                  alt="Isabel"
+                  className="pt-10"
+                />
               </div>
             </div>
             <div className="w-full">
-              <img
-                src="/img/ganadoras/isabel_obra.png"
-                style={{ width: "100%" }}
-                alt="Isabel"
-                className="pt-10"
-              />
+              <div className="pt-4">
+                <ol>
+                  <li>
+                    Título : Circuitos Híbridos Dimensiones : 410 x 100 cm{" "}
+                  </li>
+                  <li>Fecha de creación : 2007</li>
+                  <li>
+                    Técnica : 120 sistemas de audio programado y pregrabados y
+                    plancha de acrílico.
+                  </li>
+                </ol>
+              </div>
             </div>
-          </div>
-          <div className="w-full">
-            <div className="pt-10">
-              <ol>
-              <li>Título : Circuitos Híbridos Dimensiones : 410 x 100 cm </li>
-              <li>Fecha de creación : 2007</li>
-              <li>Técnica : 120 sistemas de audio programado y pregrabados y plancha de acrílico.</li>
-              </ol>
-            </div>
-          </div>
+          </div>                              
+
         </div>
 
-        <div class="icons-header-ganadoras">
+        <div class="icons-header-ganadoras ">
           <div>
             <a
               href="https://www.instagram.com/antennaorg/?hl=es-la"
