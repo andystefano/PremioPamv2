a1ee844200319a3cd947ca4ad9b392b1dda5b197
ANDY Hormazabal <andy@andy.cl>
2024-09-05 02:22:22 -0400

    ganadoras ok

--- src/components/Ganadora.js
+++ src/components/Ganadora.js
@@ -5,12 +5,12 @@ function Ganadoras(props) {
     return ( <div className="ganadora" style={{ display: props.ano === props.anoSeleccion ? 'block' : 'none' }} >
         <div>
           <div className="flex flex-col md:flex-row w-full">
-            <div className="pr-10 zona_foto">
+            <div className="pr-0 sm:pr-7 zona_foto">
 
+            <hr/>
               <img
                 src={`/img/ganadoras/${props.ano}/${props.retrato}`}
                 alt={`Retrato`}
-                style={{ width: "345px" }}
               />
             </div>
             <div>
@@ -20,7 +20,7 @@ function Ganadoras(props) {
               <h1 class="titulo_principalGanadoras w-full ">
                 { props.nombre }
               </h1>
-              <p class="text-black w-full">
+              <p class="text-black w-full text-justify text-xs sm:text-sm ">
                 { props.biografia }
               </p>
             </div>
@@ -28,14 +28,14 @@ function Ganadoras(props) {
           <div className="w-full">
             <img
               src={`/img/ganadoras/${props.ano}/${props.obra}`}
-              style={{ width: "100%" }}
+              style={{ width: "100%" , maxWidth:"65%" }}
               alt="Obra"
-              className="pt-10"
+              className="pt-4 fotoObra"
             />
           </div>
         </div>
         <div className="w-full ">
-          <div className="pt-10">
+          <div className="pt-4 text-black w-full text-justify text-xs sm:text-sm ">
             <ol>
               <li>
                 Título : Circuitos Híbridos Dimensiones : { props.titulo }
