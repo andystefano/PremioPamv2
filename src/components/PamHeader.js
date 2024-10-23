e0221ccd9f408a35598839e9aa1147790f1721d1
ANDY Hormazabal <andy@andy.cl>
2024-10-22 23:53:06 -0300

    desactivate votations

--- src/components/PamHeader.js
+++ src/components/PamHeader.js
@@ -10,10 +10,21 @@ function PamHeader({titulo,contenido}) {
       <div className="flex justify-center ">
         <div className="w-full mx-auto  container">
         <img className="HeaderIMG"  src="img/HEADER_DESK_P1.png"  alt=""/>
-        <img className="HeaderIMG"   src="img/HEADER_DESK_P2.png"  alt="" style={{cursor:'pointer'}}
-         onClick={() => window.location.href = '/votacion'} />
+        <img className="HeaderIMG"   src="img/HEADER_DESK_P2.png"  alt=""
+         style={{cursor: (process.env.REACT_APP_VOTACION_ACTIVA === '1' || process.env.REACT_APP_VOTACION_ACTIVA === 1) 
+          ? 'pointer' 
+          : 'default'}}
+         onClick={(process.env.REACT_APP_VOTACION_ACTIVA === '1' || process.env.REACT_APP_VOTACION_ACTIVA === 1) 
+          ? () => window.location.href = '/votacion' 
+          : null}
+         />
         <img id="HeaderIMG-mobile"  src="img/HEADER_MOVIL.png"  alt=""/>
-        <img id="LOGOS_MOVIL"  src="img/CONVOCATORIA_MOVIL.png"  alt=""   onClick={() => window.location.href = '/votacion'}  />          
+        <img id="LOGOS_MOVIL"  src="img/CONVOCATORIA_MOVIL.png"  alt=""   
+        onClick={(process.env.REACT_APP_VOTACION_ACTIVA === '1' || process.env.REACT_APP_VOTACION_ACTIVA === 1) 
+          ? () => window.location.href = '/votacion' 
+          : null}
+          />  
+                
         </div>
       </div>
       <div className="w-full mx-auto  container container_logo_movil">
