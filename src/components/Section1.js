7c2836fcfc883a2c6f676427e397229f436797a7
ANDY Hormazabal <andy@andy.cl>
2025-05-28 21:53:13 -0400

    update diseño

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -11,7 +11,7 @@ function Section1() {
 
   return (
     <section id="section_1" class="flex justify-center pb-5">
-      <div className="w-full mx-auto  mt-[8.7rem]" style={{backgroundColor: "#e9d9fc"}}>
+      <div className="w-full mx-auto mt-12 md:mt-[4.7rem] lg:mt-[4.7rem] xl:mt-[4.7rem] xs:px-6 px-6 md:px-0  lg:px-0  xl:px-0  md:mt-[8.7rem] lg:mt-[8.7rem] xl:mt-[8.7rem]" style={{backgroundColor: "#e9d9fc"}}>
         <div className="presentan h-full flex flex-col justify-start pl-8 ">
          <Presentadores/>                        
         </div>
@@ -22,12 +22,12 @@ function Section1() {
           A LAS MUJERES ARTISTAS EN CHILE.
           </h1>
 
-          <p className="font-inter text-[#F0000C] text-md md:text-[1.8rem] md:leading-[2.1rem]  lg:text-[1.8rem] lg:leading-[2.1rem]  xl:text-[1.8rem] xl:leading-[2.1rem]    font-semibold	md:mt-[4.4rem] lg:mt-[4.4rem] xl:mt-[4.4rem] max-w-[100%] md:max-w-[80%] lg:max-w-[80%]  xl:max-w-[80%]  px-4 md:px-0 lg:px-0 xl:px-0 ">
+          <p className="font-inter text-[#F0000C] text-[1.4rem] md:text-[1.8rem] md:leading-[2.1rem]  lg:text-[1.8rem] lg:leading-[2.1rem]  xl:text-[1.8rem] xl:leading-[2.1rem]    font-semibold	md:mt-[4.4rem] lg:mt-[4.4rem] xl:mt-[4.4rem] max-w-[100%] md:max-w-[80%] lg:max-w-[80%]  xl:max-w-[80%]  px-0 md:px-0 lg:px-0 xl:px-0 ">
           Reconocemos a mujeres artistas visuales sobre los 60 años,
           en plena vigencia creativa.
           </p>
 
-          <p className="font-inter text-[#F0000C]  text-md md:text-3xl lg:text-3xl xl:text-3xl font-ligth mt-5 max-w-[100%] md:max-w-[80%] lg:max-w-[80%]  xl:max-w-[80%] px-4 md:px-0 lg:px-0 xl:px-0 tracking-[-1px] [word-spacing:3px]">
+          <p className="font-inter text-[#F0000C] text-[1.4rem] md:text-3xl lg:text-3xl xl:text-3xl font-ligth mt-5 max-w-[100%] md:max-w-[80%] lg:max-w-[80%]  xl:max-w-[80%] px-0 md:px-0 lg:px-0 xl:px-0 tracking-[-1px] [word-spacing:3px]">
           Si eres artista, o conoces a una creadora que merezca este
           reconocimiento, este es tu momento.
           </p>
@@ -54,16 +54,16 @@ function Section1() {
 
     
 
-          <div className="md:flex md:items-start mt-[6.5rem] text-[#F0000C] ml-0 ">
+          <div className="md:flex md:items-start md:mt-[6.5rem] lg:mt-[6.5rem] xl:mt-[6.5rem] text-[#F0000C] ml-0 w-full">
   <HiOutlineArrowNarrowRight className="hidden md:block lg:block xl:block mr-1 flex-shrink-0" size={65} />
-  <h3 className="font-roboto text-xl mx-4 md:text-2xl lg:text-2xl xl:text-2xl  font-black mt-3">
+  <h3 className="font-roboto text-xl mx-0 md:mx-4 lg:mx-4 xl:mx-4 md:text-2xl lg:text-2xl xl:text-2xl  font-black mt-3">
     ¿POR QUÉ PREMIAR A MUJERES ARTISTAS SOBRE LOS 60 AÑOS?
   </h3>
 </div>
 
 
 
-        <ol  className="font-inter text-[#F0000C] text-xl md:text-2xl font-extraligth list-decimal list-inside space-y-2 md:pl-5 max-w-[90%] md:max-w-[75%] lg:max-w-[75%] xl:max-w-[75%] md:ml-12">
+        <ol  className="font-inter text-[#F0000C] text-xl md:text-2xl font-extraligth list-decimal list-inside space-y-2 md:pl-5 max-w-[100%] md:max-w-[75%] lg:max-w-[75%] xl:max-w-[75%] md:ml-12">
   <li className='pt-4'><span className='font-bold'>Porque</span> son nuestro presente, no el pasado.</li>
   <li className='pt-4'><span className='font-bold'>Porque</span> hay historias que todavía no se han contado y obras que aún esperan ser vistas.</li>
   <li className='pt-4'><span className='font-bold'>Porque</span> su camino inspira a quienes están comenzando.</li>
