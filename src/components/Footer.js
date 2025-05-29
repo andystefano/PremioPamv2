df2eccae3bedda08be48d7594ea4f5bec6510b40
ANDY Hormazabal <andy@andy.cl>
2025-05-28 23:41:21 -0400

    update diseño

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ import { TbDownload } from "react-icons/tb";
 function Footer() {
 
   return (
-    <footer  className="flex justify-center ">
+    <footer  className="md:flex lg:flex xl:flex md:justify-center lg:justify-center xl:justify-center ">
 
             
 
@@ -69,6 +69,35 @@ function Footer() {
 
 </div>
 
+
+<div className='w-full  block md:hidden lg:hidden xl:hidden' id='Footer-2-Movil'>
+
+<h1 className='mx-12' >Contáctanos</h1>
+<h1 className='mx-12' style={{fontSize: '26px', fontWeight: '300'}}>premiopam@antenna.cl</h1>
+<div className="icons-footer mx-12">
+<a href="https://www.instagram.com/antennaorg/?hl=es-la" target="_blank">
+  <FaInstagram size={32}  color="#F0000C"/>
+  </a>
+<a href="https://www.facebook.com/antennaorg" target="_blank">
+<ImFacebook size={24}  color="#F0000C"/>
+  </a>
+<a href="https://www.linkedin.com/company/antennaorg/" target="_blank">
+<AiOutlineLinkedin size={35}  color="#F0000C"/>
+  </a>
+<a href="https://www.premiopam.cl/" target="_blank"><img style={{maxHeight:'20px'}} className="hashtag__footer" src="img/icons/hashtag_1.png" alt="premio-pam"/></a>
+
+</div>
+
+<div className="icons-footer mx-12"  style={{ marginTop: '15px' }}>
+  <div style={{ display: 'flex', alignItems: 'center', marginTop: '0px'  }}>
+    <a href="https://drive.google.com/drive/folders/1S9ERKjvwknrvjhQMJxogi0Lv7QpOB7Xc?usp=drive_link " target="_blank">
+      <TbDownload size={28} color="#F0000C" /> </a>
+    <a href="https://drive.google.com/drive/folders/1S9ERKjvwknrvjhQMJxogi0Lv7QpOB7Xc?usp=drive_link " target="_blank"><span style={{ marginLeft: '8px' }} className='contenido__titulo'>Media kit 2025</span></a>
+   
+  </div>
+</div>
+
+</div>
 
     </footer>
   );
